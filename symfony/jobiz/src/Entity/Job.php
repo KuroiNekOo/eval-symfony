e223a8adc2e0324b1079a5683a4efa72a66b125d
KuroiNekOo <maximegomes.dinis@outlook.fr>
2025-06-05 11:29:28 +0200

    refactor

---
+++ symfony/jobiz/src/Entity/Job.php
@@ -0,0 +1,218 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\JobRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: JobRepository::class)]
+class Job
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $country = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $city = null;
+
+    #[ORM\Column]
+    private ?bool $remote = null;
+
+    #[ORM\Column]
+    private ?bool $allowed = null;
+
+    /**
+     * @var Collection<int, JobApplication>
+     */
+    #[ORM\OneToMany(targetEntity: JobApplication::class, mappedBy: 'job')]
+    private Collection $jobApplications;
+
+    #[ORM\ManyToOne(inversedBy: 'job')]
+    private ?Company $company = null;
+
+    /**
+     * @var Collection<int, JobCategory>
+     */
+    #[ORM\ManyToMany(targetEntity: JobCategory::class, mappedBy: 'job')]
+    private Collection $jobCategories;
+
+    #[ORM\ManyToOne(inversedBy: 'job')]
+    private ?JobType $jobType = null;
+
+    public function __construct()
+    {
+        $this->jobApplications = new ArrayCollection();
+        $this->jobCategories = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): static
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): static
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function isRemote(): ?bool
+    {
+        return $this->remote;
+    }
+
+    public function setRemote(bool $remote): static
+    {
+        $this->remote = $remote;
+
+        return $this;
+    }
+
+    public function isAllowed(): ?bool
+    {
+        return $this->allowed;
+    }
+
+    public function setAllowed(bool $allowed): static
+    {
+        $this->allowed = $allowed;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, JobApplication>
+     */
+    public function getJobApplications(): Collection
+    {
+        return $this->jobApplications;
+    }
+
+    public function addJobApplication(JobApplication $jobApplication): static
+    {
+        if (!$this->jobApplications->contains($jobApplication)) {
+            $this->jobApplications->add($jobApplication);
+            $jobApplication->setJob($this);
+        }
+
+        return $this;
+    }
+
+    public function removeJobApplication(JobApplication $jobApplication): static
+    {
+        if ($this->jobApplications->removeElement($jobApplication)) {
+            // set the owning side to null (unless already changed)
+            if ($jobApplication->getJob() === $this) {
+                $jobApplication->setJob(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getCompany(): ?Company
+    {
+        return $this->company;
+    }
+
+    public function setCompany(?Company $company): static
+    {
+        $this->company = $company;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, JobCategory>
+     */
+    public function getJobCategories(): Collection
+    {
+        return $this->jobCategories;
+    }
+
+    public function addJobCategory(JobCategory $jobCategory): static
+    {
+        if (!$this->jobCategories->contains($jobCategory)) {
+            $this->jobCategories->add($jobCategory);
+            $jobCategory->addJob($this);
+        }
+
+        return $this;
+    }
+
+    public function removeJobCategory(JobCategory $jobCategory): static
+    {
+        if ($this->jobCategories->removeElement($jobCategory)) {
+            $jobCategory->removeJob($this);
+        }
+
+        return $this;
+    }
+
+    public function getJobType(): ?JobType
+    {
+        return $this->jobType;
+    }
+
+    public function setJobType(?JobType $jobType): static
+    {
+        $this->jobType = $jobType;
+
+        return $this;
+    }
+}
